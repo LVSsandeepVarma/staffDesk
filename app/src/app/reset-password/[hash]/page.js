f04977ec7e548445d7f36111d596486ab191b6e6
= <sandeeplakham@gmail.com>
2023-06-14 14:40:31 +0530

    added use client in hash

--- app/src/app/reset-password/[hash]/page.js
+++ app/src/app/reset-password/[hash]/page.js
@@ -1,11 +1,5 @@
-"use client"
 import PasswordHash from '@/components/forgotPassword/otpPage'
-import React, { useEffect } from 'react'
-
 export default function OTP() {
-  useEffect(()=>{
-    alert("DOM rendering")
-  },[])
  
   return (
     <PasswordHash/>
